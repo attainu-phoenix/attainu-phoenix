db7176503cbde9ab72960d424d7f0e0db4f94e5c
Srinivas-Namballa <namballasrinivas902@gmail.com>
2019-02-27 04:23:53 +0530

    Created a new js file for week2 day1

---
+++ course/shallow_dive/week2/multiplication/multiplication.js
@@ -0,0 +1,41 @@
+number = 5;
+
+count = 1;
+result = count * number;
+console.log(count, ' * ' ,number, ' = ', result);
+
+count = 2;
+result = count * number;
+console.log(count, ' * ' ,number, ' = ', result);
+
+count = 3;
+result = count * number;
+console.log(count, ' * ' ,number, ' = ', result);
+
+count = 4;
+result = count * number;
+console.log(count, ' * ' ,number, ' = ', result);
+
+count = 5;
+result = count * number;
+console.log(count, ' * ' ,number, ' = ', result);
+
+count = 6;
+result = count * number;
+console.log(count, ' * ' ,number, ' = ', result);
+
+count = 7;
+result = count * number;
+console.log(count, ' * ' ,number, ' = ', result);
+
+count = 8;
+result = count * number;
+console.log(count, ' * ' ,number, ' = ', result);
+
+count = 9;
+result = count * number;
+console.log(count, ' * ' ,number, ' = ', result);
+
+count = 10;
+result = count * number;
+console.log(count, ' * ' ,number, ' = ', result);
