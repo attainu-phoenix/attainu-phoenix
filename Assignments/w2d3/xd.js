60c7b2706150222985152dc0e09eff853d2b5583
Samarth Keskar <samarthkeskar363@gmail.com>
2019-02-27 21:29:32 +0530

    JAVASCRIPT FUN

---
+++ Assignments/w2d3/xd.js
@@ -0,0 +1,15 @@
+// Hard Way 
+
+magic = document.getElementById("click");
+
+
+magic.addEventListener("click", function herewego() {
+	alert("Hello This Is The Magic Of Javascript")
+});
+
+//Easy Way
+
+
+ function magicFunction() {
+	alert("Hello This Is The Magic Of Javascript for the 2nd time");
+}
