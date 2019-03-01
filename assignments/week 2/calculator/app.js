467548a5264a819dd6752d4dcc929b3b0bff9837
luben <lubenfrank48@gmail.com>
2019-03-01 10:44:13 +0530

    completed Calculator app

--- assignments/week 2/calculator/app.js
+++ assignments/week 2/calculator/app.js
@@ -1,43 +1,24 @@
-var num1 = parseInt(document.getElementById("num1").value);
-var num2 = parseInt(document.getElementById("num2").value);
-
-var add = document.getElementById("add");
-var sub = document.getElementById("sub");
-var mul = document.getElementById("mul");
-var div = document.getElementById("divi");
-
-var result;
 func = op => {
+  let result;
+  let num1 = parseInt(document.getElementById("num1").value);
+  let num2 = parseInt(document.getElementById("num2").value);
+
+  if (isNaN(num1) || isNaN(num2)) {
+    return alert("Invalid Input.Please enter two numbers");
+  }
   switch (op) {
     case "+":
       result = num1 + num2;
-      console.log(result);
       break;
     case "-":
       result = num1 - num2;
-      console.log(result);
       break;
     case "*":
       result = num1 * num2;
-      console.log(result);
       break;
     case "/":
       result = num1 / num2;
-      console.log(result);
       break;
   }
   res.innerText = result;
 };
-
-// function adds(a) {
-//   console.log("hey");
-// }
-// add.addEventListener("click", adds);
-
-add.addEventListener("click", funct(add.value));
-
-sub.addEventListener("click", func(sub.value));
-
-mul.addEventListener("click", func(mul.value));
-
-divi.addEventListener("click", func(divi.value));
