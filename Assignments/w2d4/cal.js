77327f84baab29e2321946222638983d12515d5f
Samarth Keskar <samarthkeskar363@gmail.com>
2019-03-01 09:41:50 +0530

    right formatted code for  calculator

--- Assignments/w2d4/cal.js
+++ Assignments/w2d4/cal.js
@@ -3,12 +3,12 @@ parNum = function () {
 	number1 = parseInt(document.getElementById("num1").value);
 	number2 = parseInt(document.getElementById("num2").value);
 	resultObject = {
-		"number1":number1,
-		"number2":number2
+		"number1": number1,
+		"number2": number2
 	}
 	return resultObject;
 }
-	
+
 
 showResult = function (result) {
 	resultElement = document.getElementById("result");
@@ -24,41 +24,41 @@ addButton = document.getElementById("addButton");
 
 addButton.addEventListener("click", function addOp() {
 	getNum = parNum();
-	
-	sum = getNum.number1 + getNum.number2 ;
+
+	sum = getNum.number1 + getNum.number2;
 
 	showResult(sum);
-				
-	})
+
+})
 
 //minus 
 
 
 minButton = document.getElementById("minButton");
 minButton.addEventListener("click", function minOp() {
-		
-	    getNum = parNum();
-		diff = getNum.number1 - getNum.number2 ;
-		showResult(diff);
-				
+
+	getNum = parNum();
+	diff = getNum.number1 - getNum.number2;
+	showResult(diff);
+
 })
 
 // Multiplication
 
 function mulButton() {
-	
-		getNum = parNum();
-		mul = getNum.number1 * getNum.number2 ;
-		showResult(mul);
-		
+
+	getNum = parNum();
+	mul = getNum.number1 * getNum.number2;
+	showResult(mul);
+
 }
 
 
 //Dividation
 
 function divButton() {
-	
+
 	getNum = parNum();
-	div = getNum.number1 / getNum.number2 ;
+	div = getNum.number1 / getNum.number2;
 	showResult(div);
 }
