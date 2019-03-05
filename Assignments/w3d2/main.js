574dfa6b26b936e7653a8d1e11fc2b935f8edd47
Samarth Keskar <samarthkeskar363@gmail.com>
2019-03-05 13:48:13 +0530

    login page with html css and Javascript

--- Assignments/w3d2/main.js
+++ Assignments/w3d2/main.js
@@ -1,34 +1,34 @@
 inputCredFun = function () {
-     let username = document.getElementById("username").value;
-     let password = document.getElementById("password").value;
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
 
-     allCred = {
-         "username":username,
-         "password":password
-     }
-        return allCred ;
+    allCred = {
+        "username": username,
+        "password": password
+    }
+    return allCred;
 }
 
 
 credDb = function () {
-    let username = "sam" ;
+    let username = "sam";
     let password = "sam@123";
     returnObject = {
-        "username":username,
-        "password":password
+        "username": username,
+        "password": password
     }
     return returnObject;
 }
 
 submit = document.getElementById("submit");
 
-    submit.addEventListener("click",function mainFunction() {
-        getCred = inputCredFun();
-        getCredDb = credDb();
-    
-        if(getCred.username == getCredDb.username || getCred.password == getCredDb.password) {
-            alert("You Logged In Succesfully.");
-        } else {
-            alert ("You Entered Wrong Username Or Password.");
-        }
-    })
+submit.addEventListener("click", function mainFunction() {
+    getCred = inputCredFun();
+    getCredDb = credDb();
+
+    if (getCred.username == getCredDb.username || getCred.password == getCredDb.password) {
+        alert("You Logged In Succesfully.");
+    } else {
+        alert("You Entered Wrong Username Or Password.");
+    }
+})
