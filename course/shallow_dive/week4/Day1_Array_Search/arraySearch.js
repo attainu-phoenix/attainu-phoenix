df7883bd80240d464f7cd3d95203474ec0c05b59
Srinivas-Namballa <namballasrinivas902@gmail.com>
2019-03-11 16:08:08 +0530

    Updated Array Search file

--- course/shallow_dive/week4/Day1_Array_Search/arraySearch.js
+++ course/shallow_dive/week4/Day1_Array_Search/arraySearch.js
@@ -3,9 +3,11 @@ arr = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 searchArray = function (searchValue, arr) {
     for(count = 0; count < arr.length; count++) {
         if(arr[count] == searchValue) {
-            console.log("Element found at : " + arr[count]);
+            return count;
         }
     }
+
+    return -1;
 }
 
 searchArray(40, arr);
