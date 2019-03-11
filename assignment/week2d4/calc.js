7c4a9b85af75018e79264823c50f1a91e42b6aa9
Naresh8460 <mudaliar.naresh@gmail.com>
2019-03-11 23:00:15 +0530

    week2d4

--- assignment/week2d4/calc.js
+++ assignment/week2d4/calc.js
@@ -1,12 +1,50 @@
 plusButton = document.getElementById("add");
 plusOperation= function(){
     
+    numberValues = getValues();
+
+    sum= parseInt(numberValues.firstBlank) + parseInt(numberValues.secondBlank);
+   
+    result=document.getElementById("result");
+    result.innerText =sum;  
+}
+plusButton.addEventListener("click", plusOperation);
+
+subButton = document.getElementById("subst");
+minusOperation= function(){
+    
     firstBlank=document.getElementById("blank1");
     secondBlank=document.getElementById("blank2");
 
-    sum= parseInt(firstBlank.value) + parseInt(secondBlank.value);
+    minus = parseInt(firstBlank.value) - parseInt(secondBlank.value);
    
     result=document.getElementById("result");
-    result.innertext = sum;  
+    result.innerText =minus;  
 }
-plusButton.addEventListener("click", plusOperation);
+subButton.addEventListener("click", minusOperation);
+
+mulButton = document.getElementById("multi");
+mulOperation= function(){
+    
+    firstBlank=document.getElementById("blank1");
+    secondBlank=document.getElementById("blank2");
+
+    mul = parseInt(firstBlank.value) * parseInt(secondBlank.value);
+   
+    result=document.getElementById("result");
+    result.innerText =mul;    
+}
+mulButton.addEventListener("click", mulOperation);
+
+diviButton = document.getElementById("Div");
+diviOperation= function(){
+    
+    firstBlank=document.getElementById("blank1");
+    secondBlank=document.getElementById("blank2");
+
+    divi = parseInt(firstBlank.value) / parseInt(secondBlank.value);
+   
+    result=document.getElementById("result");
+    result.innerText =divi;    
+}
+diviButton.addEventListener ("click", diviOperation);
