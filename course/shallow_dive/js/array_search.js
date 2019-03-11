27f4222a65d434a9241d66d0efa64396b7c4e0a8
[mayurcode] <[mayur.bhandare1994@gmail.com]>
2019-03-11 12:45:59 +0530

    Array Search Assignment

---
+++ course/shallow_dive/js/array_search.js
@@ -0,0 +1,24 @@
+//ASSIGNMENT FOR ARRAY SEARCH 
+
+//INIT OF ARRAY
+var array = [1,2,3,4,5,6,7,8,9,10];
+
+
+//FUNCTION FOR SEARCH 
+/*  
+ Logic -> Loop Through the array from start to end -> if no found return true ->
+ else continue looping if end of loop
+ no not found return null ;
+*/
+function Search(arr, ele) {
+  for (var i=0; i<arr.length; i++) {
+    if (arr[i] == ele) {
+      return "Number Found at Location "+i;
+    }
+  } return null;
+}
+
+
+//Function call to run the program
+console.log(Search(array, 3)); // returns index of 3 i.e 2
+console.log(Search(array, 12)); // returns Null
