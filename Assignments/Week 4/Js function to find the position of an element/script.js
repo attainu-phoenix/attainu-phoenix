8c50b0d1f842dc9e1b92e346fd07fc81ddbe80e2
Samarth Keskar <samarthkeskar363@gmail.com>
2019-03-11 15:19:39 +0530

    Js function to find the position of an element

--- Assignments/Week 4/Js function to find the position of an element/script.js
+++ Assignments/Week 4/Js function to find the position of an element/script.js
@@ -1,14 +1,26 @@
 let arrayElements = [1, 75, 3, 45, 5, 6, 7, 93, 9, 10];
 
 
-filter = (elementToFind, arrayElements) => {
+
+filter = (userInput, arrayElements) => {
     let i;
     for (i = 0; i < arrayElements.length; i += 1) {
-        if (arrayElements[i] === elementToFind) {
+        if (arrayElements[i] === parseInt(userInput)) {
             return i;
         }
     }
     return -1;
 }
 
-filter( /* Number To Find In Array */ , arrayElements);
+listen = document.getElementById("lis");
+listen.addEventListener("click", () => {
+
+
+    result = document.getElementById("userop")
+
+    userInput = document.getElementById("user").value;
+    funresult = filter(userInput, arrayElements);
+
+
+    result.innerText = funresult;
+})
