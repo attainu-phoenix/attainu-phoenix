1f8e31fea465cb76fec69b6e8452efbe78fe9d80
abhishekmalviya17 <47751567+abhishekmalviya17@users.noreply.github.com>
2019-03-12 11:02:58 +0530

    coding challenge

--- Coding challenges/4th_week/printSquareOfOddNumbers.js
+++ Coding challenges/4th_week/printSquareOfOddNumbers.js
@@ -1,24 +1,19 @@
 //Program for priting the squares of odd Numbers from 1 to 100
-function isOdd(var n){
-	for(n=1;n<=100;n++){
-		if(n%2==0){
-		
+function isOdd(n){
+    if(n%2!=0){
+      return true;
+    }else{
       return false;
-		}
-		else{
-			return true;
-		}
-		
-	}
+    }
 }
 
-function calcSquare(){
-  for(var i=1;i<=100;i++0){
-    if(isOdd(i)==true){
-      square=i*i;
-      console.log("Square of "+i+" is "+square);
-    }
-  }
-}	
 
-calcSquare();
+function calcSq(){
+  for(i=1;i<=100;i++){
+   if(isOdd(i)==true){
+     square=i*i;
+     console.log("square of " +i+ "is "+square);
+  }
+}
+}
+calcSq();
