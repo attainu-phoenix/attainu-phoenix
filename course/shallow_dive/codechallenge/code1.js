a991340e8aeb2f214fb7aac6d97fde0e84c6e1d0
[mayurcode] <[mayur.bhandare1994@gmail.com]>
2019-03-12 09:06:38 +0530

    CODE CHALLENGES

---
+++ course/shallow_dive/codechallenge/code1.js
@@ -0,0 +1,23 @@
+/* FIND ODD NUMBER UPTO 100 AND PRINT THERE SQUARE */
+
+
+oddNumber = function(){
+	var i, odd=0;
+
+	for(i=0;i<=100;i++){
+		if(i%2!=0){
+		  odd = i;
+		  SQUARE(i);
+		}
+	}
+}
+
+
+SQUARE = function(val){
+   
+    var sq =val;
+    console.log(sq*sq);
+}
+
+
+oddNumber();
