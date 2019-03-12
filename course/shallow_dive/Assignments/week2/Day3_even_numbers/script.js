d8604816bf3c20edd19fc23e6beb07766c725f97
Srinivas-Namballa <namballasrinivas902@gmail.com>
2019-03-12 21:48:25 +0530

    deleting Assignments folder

--- course/shallow_dive/Assignments/week2/Day3_even_numbers/script.js
+++
@@ -1,5 +0,0 @@
-for(number = 100; number >= 1; number--) {
-    if(number%2 == 0) {
-        console.log(number);
-    }
-}
--- course/shallow_dive/Assignments/week2/multiplication_for_loop/script.js
+++
@@ -1,6 +0,0 @@
-number = 5;
-
-for(count = 1; count <= 10; count++) {
-    result = count * number;
-    console.log(count, ' * ', number, ' = ', result);
-}
