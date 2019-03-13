085aa21e03ea276ca848359ebd2f548cefa8da6f
shivamcs <shivamcs0049@gmail.com>
2019-03-13 18:49:57 +0530

    added comments

--- assignments/stack-emulator/script.js
+++ assignments/stack-emulator/script.js
@@ -1,5 +1,10 @@
-
+// Declare stack array which will share single instance across whole programm
+// and can be modified
 let stack = [];
+/* 
+    This function push single element one by one on above stack array
+
+*/
 const push = function(){
     let item = document.getElementById("element").value;
     if(item == ""){
@@ -10,6 +15,10 @@ const push = function(){
     console.log(stack);
     document.getElementById("stack-elements").innerHTML = stack.toString().replace (/,/g, " ")
 }
+/* 
+    This function pop single element one by one on above stack array
+
+*/
 const pop = function(){
     if(stack.length == 0){
         alert("Stack got empty no element to pop")
@@ -20,5 +29,7 @@ const pop = function(){
     document.getElementById("stack-elements").innerHTML = stack.toString().replace (/,/g, " ")
 }
 
+// handle push button 
 document.getElementById("push").addEventListener("click",push);
+// handle pop button
 document.getElementById("pop").addEventListener("click",pop);
