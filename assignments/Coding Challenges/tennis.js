42363c4406d48b7766766d7f630817714cf421f0
luben <lubenfrank48@gmail.com>
2019-03-14 09:24:17 +0530

    top 10 tennis players

---
+++ assignments/Coding Challenges/tennis.js
@@ -0,0 +1,22 @@
+var players = [
+  { name: "Roger Federer", rank: 1 },
+  { name: "Rafel Nadal", rank: 2 },
+  { name: "Nalbandian", rank: 12 },
+  { name: "Andy Murray", rank: 14 },
+  { name: "Andy Roddick", rank: 4 },
+  { name: "Pete Sampras", rank: 3 },
+  { name: "Rod Laver", rank: 190 },
+  { name: "Andre Agassi", rank: 11 },
+  { name: "Novak Djokovic", rank: 5 },
+  { name: "Arthur Ashe", rank: 8 }
+];
+
+top10players = () => {
+  for (var i in players) {
+    if (players[i].rank <= 10) {
+      console.log(players[i].name);
+    }
+  }
+};
+
+top10players();
