d359755676d84fc0d7b517100ee1d633818a45e8
abhishekmalviya17 <47751567+abhishekmalviya17@users.noreply.github.com>
2019-03-14 10:04:00 +0530

    codingchallengeday3

---
+++ Coding challenges/4th_week/objectarray/script.js
@@ -0,0 +1,41 @@
+var players = [
+    {name: "Roger Federer", rank: 1},
+    {name: "Rafel Nadal", rank: 2},
+    {name: "Nalbandian", rank: 12},
+    {name: "Andy Murray", rank: 14},
+    {name: "Andy Roddick", rank: 4},
+    {name: "Pete Sampras", rank: 3},
+    {name: "Rod Laver", rank: 190},
+    {name: "Andre Agassi", rank: 11},
+    {name: "Novak Djokovic", rank: 5},
+    {name: "Arthur Ashe", rank: 8},
+];
+
+
+var output = [];
+
+function getFields(players, field) {
+  
+    for (var i=0; i < players.length ; ++i){
+        output.push(players[i][field]);
+    
+    }
+}
+
+
+function getRankWithPlayers(){
+
+getFields(players,"rank");
+
+for(k=0;k<output.length;k++){
+    if(output[k]<=10){
+        console.log(players[k]);
+    }
+    else{
+        console.log("No player in rank 1 to 10");
+    }
+    
+}
+}
+
+getRankWithPlayers(players,"rank");
