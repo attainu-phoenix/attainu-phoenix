2e227e0b78a954ec8a7aa79000579415d45dadae
Naresh8460 <mudaliar.naresh@gmail.com>
2019-03-16 23:05:49 +0530

    week4d4.js

--- assignment/codechallenge/week4d4.js
+++ assignment/codechallenge/week4d4.js
@@ -2,6 +2,7 @@
 
 var number1=[1,2,3,18,22];
 var number2=[15,26,16,33,65,70];
+var i = [];
 
 var compareArr = function(number1, number2){
 if (number1.length !== number2.length){
@@ -10,7 +11,7 @@ if (number1.length !== number2.length){
 
 else {
  for (i=0; i<number1.length; i++) {
-    if(number1(i)!=number2(i)){
+    if(number1[i]!=number2[i]){
       return false
     }
   }
