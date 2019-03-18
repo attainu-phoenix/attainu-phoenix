a2fbe84e7330673dd887ca592985546c7788b177
shivamcs <shivamcs0049@gmail.com>
2019-03-18 09:26:29 +0530

    coding-challenge4

---
+++ assignments/coding-challenge/coding-challenge4/script.js
@@ -0,0 +1,30 @@
+'use strict';
+
+
+var printCities = function(cities){
+    for(var i=0;i<cities.length;i++){
+        
+        if(cities[i].state == 'Maharashtra' || cities[i].state == 'Gujarat'){
+            console.log(cities[i].name)    
+        }
+    }
+}
+
+var loadContent = function() {
+    
+    
+    var request = new XMLHttpRequest();
+    request.open("get", "cities.json");
+    request.send();
+    request.onreadystatechange = function(data) {
+        if(request.readyState == 4 && request.status == 200) {
+            var cities = JSON.parse(request.responseText)
+           1
+            printCities(cities);
+
+        }
+    }; 
+};
+
+var button = document.getElementById("loadButton");
+button.addEventListener("click", loadContent);
