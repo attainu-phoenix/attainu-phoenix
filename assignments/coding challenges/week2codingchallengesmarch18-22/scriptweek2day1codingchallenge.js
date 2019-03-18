00dd5e2104400b6627f3a437ef1b813150c2be9f
manwanimonishashok <manwanimonishashok@gmail.com>
2019-03-18 18:39:21 +0530

    correct code for codechallenge week2day1

--- assignments/coding challenges/week2codingchallengesmarch18-22/scriptweek2day1codingchallenge.js
+++ assignments/coding challenges/week2codingchallengesmarch18-22/scriptweek2day1codingchallenge.js
@@ -1,21 +1,24 @@
-'use strict';
+"use strict";
 
-var updateContent = function(data) {
-    for(i = 0; i < data.length; i = i + 1) {
+var updateC = function(data) {
+    for(let i = 0; i < data.length; i = i + 1) {
 
-        if(data[i].state == "Maharashtra" && data[i].state == "Gujarat") {
+        if(data[i].state == "Maharashtra" || data[i].state == "Gujarat") {
 
-            console.log(data[i].name);
-}
+            console.log(data[i].state, data[i].name);
+        }
+    }
+};
 
 
-var loadContent = function() {
+
+var loadC = function() {
 
     // Create an ajax object
     var request = new XMLHttpRequest();
 
     // Open a connect to the URL
-    request.open("get", "citiesweek2day1codingchallenge.json");
+    request.open("get","citiesweek2day1codingchallenge.json");
 
     // Send the request to the server resource.
     request.send();
@@ -24,7 +27,7 @@ var loadContent = function() {
     request.onreadystatechange = function(data) {
         if(request.readyState == 4 && request.status == 200) {
             var jsonData =  JSON.parse(request.responseText)
-            updateContent(jsonData);
+            updateC(jsonData);
         }
     }    
-}
+};
