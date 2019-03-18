3d059290dd2bf84fc66006a782e66b9fcc583bf2
abhishekmalviya17 <47751567+abhishekmalviya17@users.noreply.github.com>
2019-03-18 10:04:51 +0530

    Coding challenege ajax

---
+++ Coding challenges/5th_week/Cities_Ajax.js
@@ -0,0 +1,22 @@
+var loadContent=function() {
+	var request=new XMLHttpRequest();
+    request.open("get","cities.json");
+    request.send();
+    request.onreadystatechange=function(data) {
+        if(request.readyState==4&&request.status==200) {
+            var stateCity = JSON.parse(request.responseText)
+           logCities(stateCity);
+        }
+    }; 
+};
+
+function logCities(City) {
+    for(var i=0;i<City.length;i++) {
+        if ((City.state[i]!= Maharashtra)||(City.state[i]!=Gujarat)) {
+            continue;
+        } else {
+            console.log(City.name[i]);
+        }
+
+    }
+}
