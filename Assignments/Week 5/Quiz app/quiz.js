9245beb4028b44e2104ba26c6d970d1c080a3da9
Samarth Keskar <samarthkeskar363@gmail.com>
2019-03-19 02:02:42 +0530

    quiz app

---
+++ Assignments/Week 5/Quiz app/quiz.js
@@ -0,0 +1,69 @@
+"use strict";
+
+let data = [];
+let countdown = 30;
+let timer;
+let x;
+
+let countClock = function () {
+    let clock = document.getElementById("clock");
+    countdown--;
+    if (countdown <= 0) {
+        // disabling the submit button if timer runs out
+        document.getElementById("submit").style.display = "none";
+        clearInterval(timer);
+        return alert("So Slow. Try Another Time");
+    }
+    clock.innerText = countdown;
+};
+
+let updateContent = function (data) {
+    //timer functions
+    timer = setInterval(countClock, 1000);
+
+    // generating gibberish questions
+
+    x = Math.floor(Math.random() * 10);
+    let question = data[x]["question"];
+    let que = document.getElementById("question");
+    que.innerText = question;
+};
+
+let updateFunc = function () {
+    // create an ajax object
+    let request = new XMLHttpRequest();
+
+    request.open("get", "quiz.json");
+
+    // sennd the request to the server resources.
+    request.send();
+    // Recieve the data and do some operation
+
+    request.onreadystatechange = function (date) {
+        if (request.readyState == 4 && request.status == 200) {
+            let jsonData = JSON.parse(request.responseText);
+            data = jsonData;
+            updateContent(data);
+        }
+    };
+};
+
+updateFunc();
+
+let validation = function () {
+    let inputAns = document.getElementById("inputText").value;
+
+    //   to check if the answer is correct or wrong
+    if ((data[x]["answer"] = inputAns)) {
+        alert("Jackpot You Answered It Correctly");
+        document.getElementById("submit").style.display = "none";
+        clearInterval(timer);
+    } else {
+        document.getElementById("submit").style.display = "none";
+        alert("Too Bad Wrong Answer");
+        clearInterval(timer);
+    }
+};
+
+let submit = document.getElementById("submit");
+submit.addEventListener("click", validation);
