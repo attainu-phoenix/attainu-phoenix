06f8911145ba6fb885a32311870440b419fe23c3
Samarth Keskar <samarthkeskar363@gmail.com>
2019-03-18 09:47:07 +0530

    some changes

--- CodingChallenge/Week 5/18 march cities which only belong to the states of `Maharashtra` and `Gujarat`/script.js
+++ CodingChallenge/Week 5/18 march cities which only belong to the states of `Maharashtra` and `Gujarat`/script.js
@@ -1,33 +1,30 @@
-'use strict';
+"use strict";
 
 var updateContent = function (data) {
-    for (let i = 0; i < data.length; i++) {
-        if (data.state[i] != Maharashtra || data.state[i] != Gujarat) {
-            continue;
-        } else {
-            console.log(data.name[i])
-        }
-
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].state != "Maharashtra" || data[i].state != "Gujarat") {
+      continue;
+    } else {
+      console.log(data[i].name);
     }
-}
-
+  }
+};
 
 var loadContent = function () {
+  // Create an ajax object
+  var request = new XMLHttpRequest();
 
-    // Create an ajax object
-    var request = new XMLHttpRequest();
+  // Open a connect to a URL
+  request.open("get", "cities.json");
 
-    // Open a connect to a URL
-    request.open("get", "cities.json");
+  // Send the request to the server resource.
+  request.send();
 
-    // Send the request to the server resource.
-    request.send();
-
-    // Receive the data and do some thing with it.
-    request.onreadystatechange = function (data) {
-        if (request.readyState == 4 && request.status == 200) {
-            var jsonData = JSON.parse(request.responseText)
-            updateContent(jsonData);
-        }
-    };
+  // Receive the data and do some thing with it.
+  request.onreadystatechange = function (data) {
+    if (request.readyState == 4 && request.status == 200) {
+      var jsonData = JSON.parse(request.responseText);
+      updateContent(jsonData);
+    }
+  };
 };
