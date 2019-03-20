237741d60eec0b47fbf00ccc28de0b58bb26048b
ravi shankar <ravi shankar>
2019-03-20 14:56:25 +0530

    counter added

--- assignments/codingchallenge/week5day3-searchCity-with-A/search-cities-with-A/script.js
+++ assignments/codingchallenge/week5day3-searchCity-with-A/search-cities-with-A/script.js
@@ -1,34 +1,40 @@
 'use strict';
 
-var citiesOfState = function(cities, state) {
-    for (var i = 0; i < cities.length; i++) {
-      if(cities[i].name[0] == "A") {
-        console.log(cities[i].name);
-      }
+var loadContent = function () {
+  //create a new ajax object
+  var request = new XMLHttpRequest();
+
+  //Open a connect to a URL
+  request.open("get", "cities.json");
+
+  //Send request to the server resources.
+  request.send();
+
+  //receive the data 
+  request.onreadystatechange = function () {
+    if (request.readyState == 4 && request.status == 200) {
+      var data = JSON.parse(request.responseText);
+      console.log("Cities starting with C");
+      citiesOfState(data, "C");
+
+
     }
   };
-    
-    
-    //create a new ajax object
-      var request = new XMLHttpRequest();
-  
-    //Open a connect to a URL
-      request.open("get", "cities.json");
-  
-      //Send request to the server resources.
-      request.send();
-  
-      //receive the data 
-      request.onreadystatechange = function() {
-          if(request.readyState == 4 && request.status == 200) {
-              var data = JSON.parse(request.responseText);
-              console.log("Cities starting with A");
-              citiesOfState (data, "A");
-                
-              
-            }
-          };
-           
-  
-          
+
+};   
+       
+var counter = 0;
+var citiesOfState = function (cities, letter) {
+  for (var i = 0; i < cities.length; i++) {
+    if (cities[i].name[0] == letter) {
+      //console.log(cities[i].name);
+      counter = counter + 1;
+    }
+
+  }
+  console.log(counter);
+  return;
+};
+
+loadContent();          
   
